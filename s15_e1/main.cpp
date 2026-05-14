321fb4d176745dd67b55748c13fd56f8c866de7a
raaveinm <kirillraaveinm@gmail.com>
2026-05-14 13:00:32 +0300

    Sprint 15

---
+++ s15_e1/main.cpp
@@ -0,0 +1,37 @@
+#include "ppm_image.h"
+
+#include <iostream>
+#include <string_view>
+#include <cstddef>
+
+using namespace std;
+
+int main(int argc, const char** argv) {
+    if (argc != 3) {
+        cerr << "Usage: "sv << argv[0] << " <input image> <output image>"sv << endl;
+        return 1;
+    }
+
+    auto image = img_lib::LoadPPM(argv[1]);
+    if (!image) {
+        cerr << "Error loading image"sv << endl;
+        return 2;
+    }
+
+    // Инвертируем изображение row-by-row
+    for (int y = 0; y < image.GetHeight(); ++y) {
+        img_lib::Color* line = image.GetLine(y);
+        for (int x = 0; x < image.GetWidth(); ++x) {
+            line[x].r = std::byte(255 - std::to_integer<int>(line[x].r));
+            line[x].g = std::byte(255 - std::to_integer<int>(line[x].g));
+            line[x].b = std::byte(255 - std::to_integer<int>(line[x].b));
+        }
+    }
+
+    if (!img_lib::SavePPM(argv[2], image)) {
+        cerr << "Error saving image"sv << endl;
+        return 3;
+    }
+
+    cout << "Image saved successfully!"sv << endl;
+}
---
+++ s15_e1/ppm_image.cpp
@@ -0,0 +1,80 @@
+#include "ppm_image.h"
+
+#include <array>
+#include <fstream>
+#include <string_view>
+
+using namespace std;
+
+namespace img_lib {
+
+static constexpr string_view PPM_SIG = "P6"sv;
+static const int PPM_MAX = 255;
+
+bool SavePPM(const Path& file, const Image& image) {
+    ofstream ofs(file, ios::binary);
+    if (!ofs) {
+        return false;
+    }
+
+    ofs << PPM_SIG << '\n' << image.GetWidth() << ' ' << image.GetHeight() << '\n' << PPM_MAX << '\n';
+
+    std::vector<char> buff(image.GetWidth() * 3);
+    for (int y = 0; y < image.GetHeight(); ++y) {
+        const Color* line = image.GetLine(y);
+        for (int x = 0; x < image.GetWidth(); ++x) {
+            buff[x * 3 + 0] = static_cast<char>(line[x].r);
+            buff[x * 3 + 1] = static_cast<char>(line[x].g);
+            buff[x * 3 + 2] = static_cast<char>(line[x].b);
+        }
+        ofs.write(buff.data(), buff.size());
+    }
+
+    return ofs.good();
+
+}
+
+Image LoadPPM(const Path& file) {
+    ifstream ifs(file, ios::binary);
+    std::string sign;
+    int w, h, color_max;
+
+    ifs >> sign >> w >> h >> color_max;
+
+    if (sign != PPM_SIG || color_max != PPM_MAX) {
+        return {};
+    }
+
+    const char next = ifs.get();
+    if (next != '\n') {
+        return {};
+    }
+
+    Image result(w, h, Color::Black());
+    std::vector<char> buff(w * 3);
+
+    for (int y = 0; y < h; ++y) {
+        Color* line = result.GetLine(y);
+        ifs.read(buff.data(), w * 3);
+
+        for (int x = 0; x < w; ++x) {
+            line[x].r = static_cast<byte>(buff[x * 3 + 0]);
+            line[x].g = static_cast<byte>(buff[x * 3 + 1]);
+            line[x].b = static_cast<byte>(buff[x * 3 + 2]);
+        }
+    }
+
+    return result;
+}
+
+void NegateInplace(img_lib::Image &image) {
+    for (int y = 0; y < image.GetHeight(); ++y) {
+        Color* line = image.GetLine(y);
+        for (int x = 0; x < image.GetWidth(); ++x) {
+            line[x].r = static_cast<byte>(PPM_MAX - std::to_integer<int>(line[x].r));
+            line[x].g = static_cast<byte>(PPM_MAX - std::to_integer<int>(line[x].g));
+            line[x].b = static_cast<byte>(PPM_MAX - std::to_integer<int>(line[x].b));
+        }
+    }
+}
+}  // namespace img_lib
